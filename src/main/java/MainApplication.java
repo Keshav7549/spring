16ec802f949ab8d5da12f2ecfd45c153070bc5c7
Keshav <Keshav7549@gmail.com>
2025-08-03 03:54:28 +0530

    setup spring core project from scratch. Create src folders/sub fodlers and pom.xml

---
+++ src/main/java/MainApplication.java
@@ -0,0 +1,7 @@
+public class MainApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is a simple application for setting up Spring core project");
+    }
+}
